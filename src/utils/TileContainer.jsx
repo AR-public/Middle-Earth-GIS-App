167104e0219cdb6af741c04d71421dd5cf56cb9a
Ahmad Raja <ahmad.mujtaba.raja@gmail.com>
2022-12-28 11:52:53 +0000

    Gallery view button works

--- src/utils/TileContainer.jsx
+++ src/utils/TileContainer.jsx
@@ -3,8 +3,8 @@ import { Tile } from '../Tile'
 
 const TileContainer = ({middleEarthLoaded, isHidden}) => {
   return (
-    <div className={(isHidden ? "hide" : "") + "tile_container"}>
-      {middleEarthLoaded.map((location) => <Tile name={location.name} link={location.link} imageLink={location.imageLink} />)}
+    <div className={(isHidden ? "hide" : "") + " tile_container"}>
+      {middleEarthLoaded.map((location) => <Tile name={location.name} link={location.link} imageLink={location.imageLink}  />)}
     </div>
   )
 }
